8f6e78074c899a8839bf572c003c216cd631d881
Mike Rogers <me@mikerogers.io>
2018-01-08 10:46:15 +0000

    Planning how to lookup if page is valid or not, and how storage will work

--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -1,2 +1,19 @@
 class PageContent < ApplicationRecord
+  validates :url, presence: true
+  validate :validate_url_is_indexable, on: :create
+
+  def store_content!
+    # Use nokogiri to store JSON blob of all h1, h2, h3 & a[href] contents.
+  end
+
+  private
+
+  def validate_url_is_indexable
+    return unless url.present?
+
+    # TODO: Lookup if the URL is accessible and is a suitable format
+    if false
+      errors.add(:url, :invalid)
+    end
+  end
 end
