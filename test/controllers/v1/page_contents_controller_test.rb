ab2b520b73ff35314125dca6a09a4a2c8c81f3de
Mike Rogers <me@mikerogers.io>
2018-01-08 10:24:10 +0000

    Adding inital Integration tests

--- test/controllers/v1/page_contents_controller_test.rb
+++ test/controllers/v1/page_contents_controller_test.rb
@@ -1,14 +1,35 @@
 require 'test_helper'
 
 class V1::PageContentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test '.index - Show list of all indexed pages' do
     get v1_page_contents_index_url
     assert_response :success
+
+    # TODO - Maybe a JSON of the pages in the DB?
   end
 
-  test "should get show" do
-    get v1_page_contents_show_url
+  test '.show - Will return contents of previous indexed URL' do
+    get v1_page_contents_show_url(users(:facebook))
     assert_response :success
+
+    # TODO - Return the JSON blob perhaps?
   end
 
+  def known_url
+    'https://mikerogers.io/2018/01/05/i18n-rails-validations.html'
+  end
+
+  test '.show - Stores a valid page in the database' do
+    assert_equal PageContent.where(url: known_url).count, 0, 'Page is not in the database'
+
+    get v1_page_contents_show_url(known_url)
+    assert_response :success
+
+    assert_equal PageContent.where(url: known_url).count, 1, 'Page is in the database'
+  end
+
+  test '.show - Will gracefully 404 when a URL does not exist' do
+    get v1_page_contents_show_url('http://www.mikerogers.io/i-dont-exist')
+    assert_response :not_found
+  end
 end
